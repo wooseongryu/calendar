c6eb8889b51294341f4b506319118545d9222a91
us3754 <useong3754@gmail.com>
2022-06-19 17:17:36 +0900

    달 최대 일수

--- src/ryu/calendar/Calendar.java
+++ src/ryu/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package ryu.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
 		System.out.println(" 월 화 수  목 금  토  일");
@@ -8,5 +10,12 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		System.out.printf("달을 입력하세요.\n");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		int[] cal = new int[] {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.printf("%d월은 %d일 까지 있습니다.\n", month, cal[month]);
+		scanner.close();
 	}
 }
