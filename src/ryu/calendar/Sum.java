809f9a7e8aad919f7be27c5b970bd3a2b2a15a08
us3754 <useong3754@gmail.com>
2022-06-19 16:56:52 +0900

    다시 수정

--- src/ryu/calendar/Sum.java
+++ src/ryu/calendar/Sum.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class Sum {
 	public static void main(String[] args) {
-		System.out.println("두 수를 입력하세요 : ");
+		int a, b;
+		String s1, s2;
+		
+		System.out.println("두 수를 입력해 주세요 : ");		
 		Scanner scanner = new Scanner(System.in);
-		String number = scanner.nextLine();
-		String[] splitedValue = number.split(" ");
-		int first = Integer.parseInt(splitedValue[0]);
-		int second = Integer.parseInt(splitedValue[1]);
-		System.out.println("두 수의 합은 " + (first + second) + "입니다.");
+		s1 = scanner.next();
+		s2 = scanner.next();
+		a = Integer.parseInt(s1);
+		b = Integer.parseInt(s2);		
+		System.out.printf("%d와 %d의 합은 %d입니다.\n", a, b, a + b);		
+		scanner.close();
 	}
 }
