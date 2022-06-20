9307682846ccaf498c81dd26442792b4d45001aa
us3754 <useong3754@gmail.com>
2022-06-20 17:54:48 +0900

    달력 반복문으로 출력 수정

--- src/ryu/calendar/CalPrint.java
+++ src/ryu/calendar/CalPrint.java
@@ -5,54 +5,27 @@ import java.util.Scanner;
 // 28 --- 2
 // 30 --- 4 6 9 11
 public class CalPrint {
+	private static final int[] MaxDaysOfMonth =  {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public static int MaxDay(int month) {
+		return MaxDaysOfMonth[month - 1];
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("종료하려면 -1을 입력하세요.");
-		while (true) {
-			System.out.println("월을 입력하세요.");
-			System.out.print("> ");
-			Scanner scanner = new Scanner(System.in);
-			int month = scanner.nextInt();
-			if (month == 2) {
-				calendar28();
-			} else if (month == 4 || month == 6 || month == 9 || month == 11) {
-				calendar30();
-			} else if (month == -1) {
-				System.out.println("Bye");
-				break;
-			} else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-				calendar31();
-			} else {
-				System.out.println("1에서 12사이의 값을 입력해 주세요.");
+		System.out.println("월을 입력하세요.");
+		System.out.print("> ");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		int MaxDay = MaxDay(month);
+		
+		System.out.println(" 월 화 수  목 금  토  일");
+		System.out.println("--------------------");
+		for (int i = 1; i <= MaxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == 0) {
+				System.out.println();
 			}
 		}
 	}
-	
-	public static void calendar31() {
-		System.out.println(" 일 화 수  목 금  토  월");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-		System.out.println("29 30 31");
-	}
-	
-	public static void calendar28() {
-		System.out.println(" 일 화 수  목 금  토  월");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
-	
-	public static void calendar30() {
-		System.out.println(" 일 화 수  목 금  토  월");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-		System.out.println("29 30");
-	}
+
 }
